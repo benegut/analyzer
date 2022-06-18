9a7612453433f718144285d7fc50a205e90e98f5
benegut <bgutsche@gmx.de>
2022-06-19 01:14:08 +0200

    added remove and hide

--- window.cpp
+++ window.cpp
@@ -1,11 +1,14 @@
 #include "window.hpp"
 #include <iostream>
-
+#include <string>
+#include <sstream>
+#include <fstream>
 
 Window::Window()
   : timePlot(new QCustomPlot)
   , xyPlot(new QCustomPlot)
   , colorMap(new QCPColorMap(xyPlot->xAxis, xyPlot->yAxis))
+  , plotcontextmenu(new PlotContextMenu)
 {
   QSplitter * splitter = new QSplitter;
   splitter->addWidget(timePlot);
@@ -13,6 +16,10 @@ Window::Window()
   timePlot->show();
   xyPlot->show();
   setCentralWidget(splitter);
+
+  timePlot->setInteractions(QCP::iSelectPlottables);
+  connect(timePlot, &QCustomPlot::plottableClick, this, &Window::plottableClick_slot);
+  connect(timePlot, &QCustomPlot::mouseDoubleClick, this, &Window::mouseDoubleClick_slot);
 
   timePlot->axisRect()->setupFullAxesBox(true);
   timePlot->setInteraction(QCP::iRangeZoom, true);
@@ -30,8 +37,6 @@ Window::Window()
 
   resize(1700, 800);
   this->show();
-
-  QString fileName = QFileDialog::getOpenFileName(this, tr("&Open File"),"/home/avs-es/Documents/C++", tr("*.txt *csv"));
 }
 
 
@@ -41,23 +46,23 @@ void Window::actions()
   file = menuBar()->addMenu(tr("&File"));
 
   QAction * open_action = new QAction(tr("&Open"));
-  connect(open_action, &QAction::triggered, [this](){std::cout << "clicked\n";});
+  connect(open_action, &QAction::triggered, this, &Window::open_action_slot);
   file->addAction(open_action);
 
   QAction * save_action = new QAction(tr("&Save"));
-  connect(save_action, &QAction::triggered, [this](){std::cout << "clicked\n";});
+  connect(save_action, &QAction::triggered, this, &Window::save_action_slot);
   file->addAction(save_action);
 
   QAction * save_as_action = new QAction(tr("&Save As"));
-  connect(save_as_action, &QAction::triggered, [this](){std::cout << "clicked\n";});
+  connect(save_as_action, &QAction::triggered, this, &Window::save_as_action_slot);
   file->addAction(save_as_action);
 
   QAction * import_action = new QAction(tr("&Import"));
-  connect(import_action, &QAction::triggered, [this](){std::cout << "clicked\n";});
+  connect(import_action, &QAction::triggered, this, &Window::import_action_slot);
   file->addAction(import_action);
 
   QAction * export_action = new QAction(tr("&Export"));
-  connect(export_action, &QAction::triggered, [this](){std::cout << "clicked\n";});
+  connect(export_action, &QAction::triggered, this, &Window::export_action_slot);
   file->addAction(export_action);
 
 
@@ -81,8 +86,128 @@ void Window::actions()
   tools = menuBar()->addMenu(tr("&Tools"));
 
   QAction * math_action = new QAction(tr("&Math"));
-  connect(math_action, &QAction::triggered, [this](){std::cout << "math activated\n";});
+  connect(math_action, &QAction::triggered, this, &Window::math_action_slot);
   tools->addAction(math_action);
+}
+
+
+void Window::open_action_slot()
+{
+  QString filename = QFileDialog::getOpenFileName(this, tr("&Open File"),"/home/avs-es", tr("*.analyzer"));
+  if(filename != "")
+    std::cout << "Not supported yet! Use Import.";
+}
+
+
+void Window::save_action_slot()
+{
+}
+
+
+void Window::save_as_action_slot()
+{
+}
+
+
+void Window::import_action_slot()
+{
+  std::string filename = QFileDialog::getOpenFileName(this, tr("Open File"),"/home/koyomi/Documents/data", tr("*.txt *csv")).toStdString();
+  if(filename != "")
+    {
+      AskForHeader dialog;
+      int headers = dialog.get_value();
+
+      load_file(filename,headers);
+    }
+  timePlot->replot();
+}
+
+
+void Window::load_file(std::string filename, int headers)
+{
+  std::ifstream file(filename);
+  std::string line;
+
+  getline(file,line);
+  std::stringstream ss1(line);
+  std::string number1;
+  int columnCount = 0;
+  while(getline(ss1,number1,'\t'))
+    columnCount++;
+
+  file.clear();
+  file.seekg(0);
+
+  int rowCount = 0;
+  while(getline(file,line))
+    rowCount++;
+
+  for(int j=0;j<columnCount;j++)
+    timePlot->addGraph();
+
+  file.clear();
+  file.seekg(0);
+
+  int counter = 0;
+  for(int i=0;i<headers;i++)
+    getline(file, line);
+
+  while(getline(file, line) && counter < 1000000000)
+    {
+      std::stringstream ss(line);
+      std::string number;
+      int j = 0;
+      while(getline(ss,number,'\t'))
+        {
+          timePlot->graph(j)->addData((double)counter,std::stod(number));
+          timePlot->graph(j)->setPen(QPen(QColor((Qt::GlobalColor)(7+j))));
+          timePlot->graph(j)->setSelectable(QCP::stWhole);
+          j++;
+        }
+      counter++;
+    }
+}
+
+
+void Window::export_action_slot()
+{
+}
+
+
+void Window::math_action_slot()
+{
+}
+
+
+void Window::plottableClick_slot(QCPAbstractPlottable * plottable,
+                                 int                    dataIndex,
+                                 QMouseEvent *          event)
+{
+  switch(event->button())
+    {
+    case Qt::RightButton :
+      if(plottable->selected())
+        {
+          plotcontextmenu->set_plottable(plottable);
+          plotcontextmenu->move(event->globalPos());
+          plotcontextmenu->show();
+        }
+      break;
+
+    case Qt::LeftButton :
+      std::cout << "ch3ck\n"; break;
+    default:
+      break;
+    }
+}
+
+
+void Window::mouseDoubleClick_slot(QMouseEvent * event)
+{
+  if(event->button() == Qt::RightButton)
+    for(int i=0; i<timePlot->graphCount();i++)
+      timePlot->graph(i)->setVisible(true);
+  timePlot->replot();
 }
 
 
@@ -90,4 +215,71 @@ void Window::contextMenuEvent(QContextMenuEvent *event)
 {
   QMenu menu(this);
   menu.exec(event->globalPos());
+}
+
+
+AskForHeader::AskForHeader()
+{
+  QLabel *        label = new QLabel(this);
+  label->setText("How many headers?");
+  QLineEdit *     enter = new QLineEdit(this);
+  connect(enter, &QLineEdit::editingFinished, [this,enter](){headers_value = enter->text().toInt();done(1);});
+  enter->setText("1");
+  QVBoxLayout *   layout = new QVBoxLayout(this);
+  layout->addWidget(label);
+  layout->addWidget(enter);
+  setLayout(layout);
+  exec();
+}
+
+
+int AskForHeader::get_value()
+{
+  return headers_value;
+}
+
+
+PlotContextMenu::PlotContextMenu()
+{
+  QAction * copy_action = new QAction(tr("&Copy"));
+  addAction(copy_action);
+  connect(copy_action, &QAction::triggered, this, &PlotContextMenu::copy_action_slot);
+
+  QAction * truncate_action = new QAction(tr("&Truncate"));
+  addAction(truncate_action);
+  connect(truncate_action, &QAction::triggered, this, &PlotContextMenu::truncate_action_slot);
+
+  QAction * filter_action = new QAction(tr("&Filter"));
+  addAction(filter_action);
+  connect(filter_action, &QAction::triggered, this, &PlotContextMenu::filter_action_slot);
+
+  QAction * hide_action = new QAction(tr("&Hide"));
+  addAction(hide_action);
+  connect(hide_action, &QAction::triggered, [this](){plottable->setVisible(false);plottable->parentPlot()->replot();});
+
+  QAction * remove_action = new QAction(tr("&Remove"));
+  addAction(remove_action);
+  connect(remove_action, &QAction::triggered, [this](){plottable->parentPlot()->removePlottable(plottable);plottable->parentPlot()->replot();});
+
+}
+
+
+void PlotContextMenu::set_plottable(QCPAbstractPlottable * p)
+{
+  plottable = p;
+}
+
+
+void PlotContextMenu::copy_action_slot()
+{
+}
+
+
+void PlotContextMenu::truncate_action_slot()
+{
+}
+
+
+void PlotContextMenu::filter_action_slot()
+{
 }
--- window.hpp
+++ window.hpp
@@ -4,8 +4,38 @@
 
 #include <QMainWindow>
 #include "qcustomplot.h"
+#include <string>
 
 
+class PlotContextMenu : public QMenu
+{
+  Q_OBJECT
+
+public:
+  PlotContextMenu();
+  void set_plottable(QCPAbstractPlottable *);
+
+private:
+  QCPAbstractPlottable *  plottable;
+
+public slots:
+  void                    copy_action_slot();
+  void                    truncate_action_slot();
+  void                    filter_action_slot();
+};
+
+
+class AskForHeader : public QDialog
+{
+  Q_OBJECT
+
+public:
+  AskForHeader();
+  int get_value();
+
+private:
+  int headers_value;
+};
 
 
 class Window : public QMainWindow
@@ -19,8 +49,25 @@ public:
   QCustomPlot *           xyPlot;
   QCPColorMap *           colorMap;
 
+
+public slots:
+  void                    open_action_slot();
+  void                    save_action_slot();
+  void                    save_as_action_slot();
+  void                    import_action_slot();
+  void                    export_action_slot();
+  void                    math_action_slot();
+  void                    plottableClick_slot(QCPAbstractPlottable *,
+                                              int,
+                                              QMouseEvent *);
+  void                    mouseDoubleClick_slot(QMouseEvent *);
+
+  void                    load_file(std::string, int);
+
 private:
   void                    actions();
+
+  PlotContextMenu *       plotcontextmenu;
 
 protected:
   void contextMenuEvent(QContextMenuEvent *event) override;
