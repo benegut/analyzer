a85180f1d6a97a16b0aeacd7a92c3215158e8a45
benegut <bgutsche@gmx.de>
2022-07-22 03:15:34 +0200

    filter is working now

--- filter.cpp
+++ filter.cpp
@@ -6,23 +6,39 @@
 
 void PlotContextMenu::filter_action_slot()
 {
+  int size = (((QCPGraph *)plottable)->data())->size();
+
   QVector<double> key_vec;
-  key_vec.reserve((((QCPGraph *)plottable)->data())->size());
-  kfr::univector<double> uni_vec;
-  std::vector<double> vec;
-  vec.reserve((((QCPGraph *)plottable)->data())->size());
-  uni_vec.reserve((((QCPGraph *)plottable)->data())->size());
+  key_vec.reserve(size);
+
+  kfr::univector<double> data;
+  data.reserve(size);
+
+  kfr::univector<double, 7> taps;
+
+  kfr::expression_pointer<double> kaiser = kfr::to_pointer(kfr::window_kaiser(taps.size(), 3.0));
+  fir_lowpass(taps, 0.00001, kaiser, true);
+
+
+  kfr::filter_fir<double> filter(taps);
+
   auto begin = (((QCPGraph *)plottable)->data())->constBegin();
   auto end   = (((QCPGraph *)plottable)->data())->constEnd();
+
   for(auto itr = begin; itr != end; itr++)
     {
-      uni_vec.push_back(itr->value);
+      data.push_back(itr->value);
       key_vec.push_back(itr->key);
     }
-  kfr::expression_pointer<double> rectangular = kfr::to_pointer(kfr::window_rectangular(uni_vec.size()));
-  kfr::fir_lowpass(uni_vec, 0.00008 ,rectangular,false);
-  for(auto i : uni_vec)
+
+  filter.apply(data);
+
+  std::vector<double> vec;
+  vec.reserve(size);
+
+  for(auto i : data)
     vec.push_back(i);
+
   ((QCPGraph *)plottable)->setData(key_vec, QVector<double>::fromStdVector(vec));
   parent->replot();
 }
